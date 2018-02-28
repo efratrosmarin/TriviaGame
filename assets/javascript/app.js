5f2284ee09eb3330af8f6f8c04d9ed20c9a9bdd5
Efrat Rosmarin <efratedit@gmail.com>
2018-02-27 21:56:07 -0500

    contdown

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,33 @@
-$("#exampleRadios1")
+$(document).ready(function(){
+var counter
+    
+    $(".startClock").click( function(){
+       
+            counter = 59;
+          
+            setInterval(function() {
+              counter--;
+              if (counter >= 0) {
+                span = document.getElementById("count");
+                span.innerHTML = counter;
+              }
+              // Display 'counter' wherever you want to display it.
+              if (counter === 0) {
+              //    alert('this is where it happens');
+                  clearInterval(counter);
+              }
+          
+            }, 1000);
+          
+          })();
+
+
+
+ });
+
+
+
+
+
+
+
